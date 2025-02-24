887fd878033c12cbe206048c46837b175d92f684
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-24 14:00:40 -0300

    feat: add shadcn initial configs

---
+++ src/app/products/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const Products = () => {
+    return ( 
+        <div className="p-5 rounded-xl bg-red-500">
+
+            <h1>Products</h1> 
+            <Button>Teste</Button>
+            <Input placeholder="Teste" />
+        </div>
+    );
+}
+ 
+export default Products;
