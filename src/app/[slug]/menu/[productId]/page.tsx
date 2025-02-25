f2e19ac44ed86ac5de0b61cab2604a00d6fba6e9
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-25 18:16:10 -0300

    feat: add ProductPage component to display product details with 404 handling

---
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,32 @@
+// import { notFound } from "next/navigation";
+
+import NotFoundPage from "@/components/not-found";
+import { getProductsById } from "@/data/get-products-by-id";
+
+import ProductHeader from "./components/product-header";
+import ProductDetails from "./components/products-details";
+
+interface ProductPageProps {
+    params: Promise<{ slug: string; productId: string }>;
+}
+
+const ProductPage = async ({ params } : ProductPageProps) => {
+    const { slug, productId } = await params;
+    const product = await getProductsById(productId);
+    if (!product) {
+        return <NotFoundPage />;
+    }
+
+    if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+        return <NotFoundPage />;
+    }
+
+    return ( 
+        <div className="flex flex-col h-full">
+            <ProductHeader product={product} />
+            <ProductDetails product={product} />
+        </div> 
+    );
+}
+ 
+export default ProductPage;
