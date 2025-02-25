a3da6fac751844187cf51f5acd838103c627fb67
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-25 12:23:31 -0300

    refactor: integrate category and product retrieval in restaurant menu page

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 
+import { getCategoriesByRestaurant } from "@/data/get-categories-by-restaurant";
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
 
 import RestaurantCategories from "./components/categories";
@@ -18,21 +19,33 @@ const RestaurantMenuPage = async ({
   params,
   searchParams,
 }: RestaurantMenuPageProps) => {
+  // Pega o slug do restaurante e o método de consumo da URL
   const { slug } = await params;
   const { consumptionMethod } = await searchParams;
   if (!isConsumptionMethodValid(consumptionMethod)) {
     return notFound();
   }
 
+  // Busca o restaurante pelo slug
   const restaurant = await getRestaurantBySlug(slug);
   if (!restaurant) {
     return notFound();
   }
 
+  // Busca as categorias e produtos do restaurante
+  const categoriesAndProducts = await getCategoriesByRestaurant({ restaurantId: restaurant.id, withProducts: true });
+
+  console.log(categoriesAndProducts);
+
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
-      <RestaurantCategories restaurant={restaurant} />
+      {/* Renderiza as categorias e produtos do restaurante */}
+      {categoriesAndProducts ? (
+        <RestaurantCategories restaurant={restaurant} categories={categoriesAndProducts} />
+      ) : (
+        <p>Este restaurante não possui categorias ou produtos</p>
+      )}
     </div>
   );
 };
