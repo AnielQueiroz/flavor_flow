022994a458e12c5a8281b3d13a2b408417e7d71c
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-25 13:24:25 -0300

    feat: add header with logo and menu button to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import "./globals.css";
 
+import { MenuIcon } from "lucide-react";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -23,6 +27,20 @@ export default function RootLayout({
       <body
         className={`${poppins.className} antialiased`}
       >
+      
+          <header className="flex items-center justify-between bg-white shadow-sm py-4">
+            <div className="flex items-center">
+              <Image src="/logo.png" alt="Flavor Flow" width={50} height={50} />
+              <h1 className="text-xl font-semibold">Flavor Flow</h1>
+            </div>
+            <div className="pr-4">
+              <Button variant="secondary" size="sm">
+                <MenuIcon size={24} />
+              </Button>
+            </div>
+          </header>
+          
+   
         {children}
       </body>
     </html>
